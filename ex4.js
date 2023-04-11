e648c89a56b007b8349685076df9ea2038da5457
Justin Tan <128333336+nameisJustinT@users.noreply.github.com>
2023-04-11 15:15:54 +0800

    Update ex4.js

--- ex4.js
+++ ex4.js
@@ -5,3 +5,16 @@
 
 
 
+console.log("Numbers divisible by 5: ");
+for(let i=1; i<=100; i++) {
+    if(i%5 === 0) {
+        console.log(i);
+    }
+}
+
+for(let i=0 ; i<3;i++) {
+  console.log("I am the outer loop");
+  for(let j=0;j<3;j++) {
+      console.log("I am the inner loop");
+  }
+}
